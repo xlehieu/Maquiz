62a264ce526e9c61664af653cba2e29f8b499841
xlehieu <xlehieu@gmail.com>
2025-02-26 10:26:47 +0700

    edit env

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-const uri =
-    'mongodb+srv://admin:t5fUEiyxf6UbY2MN@cluster0.tnl6qbx.mongodb.net/mern-learning?retryWrites=true&w=majority&appName=Cluster0';
-const uriCompass = 'mongodb://localhost:27017/AHAuth';
+dotenv.config();
+const uri = process.env.DATABASE_URI;
 
 export async function connect() {
     try {
-        await mongoose.connect(uriCompass);
+        await mongoose.connect(String(uri));
         console.log('Successfully connect');
-    } catch (err) {
-        console.log(err);
-    }
+    } catch (err) {}
 }
